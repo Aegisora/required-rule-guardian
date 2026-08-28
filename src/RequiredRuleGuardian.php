6ebecae902857ace7fcfc3dc9d68090fd3b673fc
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2026-08-28 19:23:55 +0400

    [AEGISORA-69318556] Injected guardian.

--- src/RequiredRuleGuardian.php
+++ src/RequiredRuleGuardian.php
@@ -2,6 +2,15 @@
 
 namespace Aegisora\RuleGuardians\RequiredRule;
 
+use Aegisora\Guardian\Guardian;
+
 class RequiredRuleGuardian
 {
+    private Guardian $guardian;
+
+    public function __construct(
+        Guardian $guardian
+    ) {
+        $this->guardian = $guardian;
+    }
 }
