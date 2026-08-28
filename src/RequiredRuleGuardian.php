c45914bcf24095e7bba094abb3d8b5c30afd037f
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2026-08-28 19:25:22 +0400

    [AEGISORA-69318556] Implemented check method.

--- src/RequiredRuleGuardian.php
+++ src/RequiredRuleGuardian.php
@@ -2,7 +2,11 @@
 
 namespace Aegisora\RuleGuardians\RequiredRule;
 
+use Aegisora\Guardian\Exceptions\GuardianExecutingRuleException;
+use Aegisora\Guardian\Exceptions\GuardianValidationException;
 use Aegisora\Guardian\Guardian;
+use Aegisora\Rules\RequiredRule;
+use Throwable;
 
 class RequiredRuleGuardian
 {
@@ -12,5 +16,18 @@ class RequiredRuleGuardian
         Guardian $guardian
     ) {
         $this->guardian = $guardian;
+    }
+
+    /**
+     * @param mixed $value
+     * @throws GuardianExecutingRuleException
+     * @throws GuardianValidationException
+     * @throws Throwable
+     */
+    public function check(
+        $value,
+        ?Throwable $exception = null
+    ): void {
+        $this->guardian->check($value, RequiredRule::create(), $exception);
     }
 }
